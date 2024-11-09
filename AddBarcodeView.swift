970afd764a4033a2c43da56fefdf47b89d1a6830
Bastiaan Quast <bquast@valiq.com>
2024-11-09 09:17:25 +0100

    add help info

--- AddBarcodeView.swift
+++ AddBarcodeView.swift
@@ -6,16 +6,40 @@ struct AddBarcodeView: View {
     @Environment(\.presentationMode) private var presentationMode
     @State private var label: String = ""
     @State private var data: String = ""
+    @State private var showingLabelHelp = false
+    @State private var showingDataHelp = false
 
     var body: some View {
         NavigationView {
             Form {
-                Section(header: Text("Label")) {
-                    TextField("Enter label", text: $label)
-                }
-                Section(header: Text("Data")) {
-                    TextField("Enter 12-digit barcode data", text: $data)
-                        .keyboardType(.numberPad)
+                Section {
+                    HStack {
+                        TextField("Label", text: $label)
+                        Button {
+                            showingLabelHelp.toggle()
+                        } label: {
+                            Image(systemName: "questionmark.circle")
+                                .foregroundColor(.gray)
+                        }
+                        .popover(isPresented: $showingLabelHelp) {
+                            Text("Enter to label to remember the barcode, such as the store name.")
+                                .padding()
+                        }
+                    }
+
+                    HStack {
+                        TextField("Data", text: $data)
+                        Button {
+                            showingDataHelp.toggle()
+                        } label: {
+                            Image(systemName: "questionmark.circle")
+                                .foregroundColor(.gray)
+                        }
+                        .popover(isPresented: $showingDataHelp) {
+                            Text("Enter the 13-digit barcode.")
+                                .padding()
+                        }
+                    }
                 }
             }
             .navigationBarTitle("Add Barcode")
