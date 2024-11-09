010ec5812e333d0ba196953ac3f8d56bb66e8314
Bastiaan Quast <bquast@valiq.com>
2024-11-09 11:32:25 +0100

    simplify, works better

--- QRCodeGenerator.swift
+++ QRCodeGenerator.swift
@@ -2,23 +2,19 @@ import SwiftUI
 import CoreImage.CIFilterBuiltins
 
 struct QRCodeGenerator {
-    static func draw(_ string: String) -> some View {
+    static func draw(_ string: String) -> Image {
         let context = CIContext()
         let filter = CIFilter.qrCodeGenerator()
         filter.setValue(string.data(using: .utf8), forKey: "inputMessage")
         
         if let outputImage = filter.outputImage {
-            let scale = CGAffineTransform(scaleX: 6, y: 6)
+            let scale = CGAffineTransform(scaleX: 5, y: 5)
             let scaledImage = outputImage.transformed(by: scale)
             if let cgImage = context.createCGImage(scaledImage, from: scaledImage.extent) {
                 return Image(cgImage, scale: 1.0, label: Text("QR Code"))
-                    .interpolation(.none)
-                    .fixedSize()
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
         }
         
         return Image(systemName: "xmark.circle")
-            .foregroundColor(.red)
     }
 } 
